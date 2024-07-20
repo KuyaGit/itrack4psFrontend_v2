e6788936b63398ee68801f511e2b7c257b4ae6de
Mark Lowel Montealto <102800815+KuyaGit@users.noreply.github.com>
2024-07-21 00:04:03 +0800

    comment

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-
+// Just Comment
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
